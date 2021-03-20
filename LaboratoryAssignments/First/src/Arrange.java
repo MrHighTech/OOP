c5afef5cff66eabe5d00290899d9433505c0ed90
Mateo Bencek <mateobencek007@gmail.com>
2021-03-20 11:21:03 +0100

    Minor changes in the comments of the code

--- LaboratoryAssignments/First/src/Arrange.java
+++ LaboratoryAssignments/First/src/Arrange.java
@@ -6,8 +6,8 @@ public class Arrange {
         System.out.println(Arrays.toString(arrangeOddEven(new int[]{3, 4, 78, 2, 1, 7})));
     }
 
-    // Sort so odd numbers are on odd indexes, and even on even indexes considering it will be same number of
-    // odd and even numbers
+    // Sort so odd numbers are on odd indexes, and even on even indexes
+    // considering it will be same number of odd and even numbers
     public static int[] arrangeOddEven(int[] arr) {
         int length = arr.length;
         int temp;
